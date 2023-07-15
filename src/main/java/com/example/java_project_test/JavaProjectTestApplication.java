2b2af1639ff25f0edfe7ac604dbaed277ff82742
sriram.ramesh <sriram.ramesh15@gmail.com>
2023-07-15 18:43:46 +0530

    Initial commit

---
+++ src/main/java/com/example/java_project_test/JavaProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_project_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectTestApplication.class, args);
+	}
+
+}
