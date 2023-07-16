328f0d0fb95c97455ca58406753eb8b923885b62
sriram.ramesh <sriram.ramesh15@gmail.com>
2023-07-16 10:38:22 +0530

    "commiting further rest api changes"

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,8 +1,12 @@
 import com.example.java_project_test.Userservice.UserserviceImpl;
 import com.example.java_project_test.entity.User;
+import com.example.java_project_test.repo.Userrepo;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -10,14 +14,24 @@ public class UserController {
     @Autowired
     private UserserviceImpl userservice;
 
+    @Autowired
+    private Userrepo repo;
+
     @PostMapping("/SaveUser")
     public ResponseEntity<String> saveUser(@RequestBody User user) {
         return userservice.getUsers(user);
+    }
+
+
+    @GetMapping("/GetUser")
+    public List<User> getUser() {
+        return repo.findAll();
+        }
 
 
     }
 
-}
+
 
 
 
